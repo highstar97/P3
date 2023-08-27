01373b9f005b09f68ca6a31a2fc3e26048e7c2e3
highstar97 <pgt0185@gmail.com>
2023-08-27 17:38:55 +0900

    [Setting] 폴더 정리
    * 폴더 정리
    * include 경로 설정

--- Source/P3/P3.Build.cs
+++ Source/P3/P3.Build.cs
@@ -9,5 +9,13 @@ public class P3 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG" });
-	}
+
+        PrivateIncludePaths.Add("P3/Public/Buff");
+        PrivateIncludePaths.Add("P3/Public/Character");
+        PrivateIncludePaths.Add("P3/Public/Character/AnimInstance");
+        PrivateIncludePaths.Add("P3/Public/Character/Controller");
+        PrivateIncludePaths.Add("P3/Public/Component");
+        PrivateIncludePaths.Add("P3/Public/UI");
+        PrivateIncludePaths.Add("P3/Public/Weapon");
+    }
 }
